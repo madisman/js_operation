ce9fa8ddba5e09e87ac1186f55ed86dfe6dbb570
madisman <674300607@qq.com>
2018-12-10 11:03:36 +0800

    更新新的使用方法

--- index.js
+++ index.js
@@ -1,26 +1,56 @@
+class Compute {
+    constructor(initNum) {
+        this.result = initNum ? initNum : 0;
+    }
+    // 加
+    add(num) {
+        if (!num) {
+            return this;
+        }
+        const long = getLong(this.result, num);
+        this.result = (this.result * (10 ** long) + num * (10 ** long)) / (10 ** long);
+        return this;
+    }
+    // 减
+    sub(num) {
+        if (!num) {
+            return this;
+        }
+        const long = getLong(this.result, num);
+        this.result = (this.result * (10 ** long) - num * (10 ** long)) / (10 ** long);
+        return this;
+    }
+    // 乘
+    mul(num) {
+        if (!num) {
+            return this;
+        }
+        const long = getLong(this.result, num);
+        this.result = (this.result * (10 ** long) * (num * (10 ** long))) / (10 ** long) / (10 ** long);
+        return this;
+    }
+    // 除
+    div(num) {
+        if (!num) {
+            return this;
+        }
+        const long = getLong(this.result, num);
+        this.result = (this.result * (10 ** long) / (num * (10 ** long)));
+        return this;
+    }
+    getValue() {
+        return this.result;
+    }
+}
 function getLong(a, b) {
+    if (a < 0) {
+        a = Number(a.toString().substring(1));
+    }
+    if (b < 0) {
+        b = Number(b.toString().substring(1));
+    }
     const _aln = a.toString().split('.')[1] ? a.toString().split('.')[1].length : 0;
     const _bln = b.toString().split('.')[1] ? b.toString().split('.')[1].length : 0;
     const long = _aln > _bln ? _aln : _bln;
     return long;
-}
-// 加
-function sum(a, b) {
-    const long = getLong(a, b);
-    return (a * (10 ** long) + b * (10 ** long)) / (10**long);
-}
-// 减
-function sub(a, b) {
-    const long = getLong(a, b);
-    return (a * (10 ** long) - b * (10 ** long)) / (10**long);
-}
-// 乘
-function mul(a, b) {
-    const long = getLong(a, b);
-    return (a * (10 ** long) * (b * (10 ** long))) / (10**long) / (10**long);
-}
-// 除
-function div(a, b) {
-    const long = getLong(a, b);
-    return (a * (10 ** long) / (b * (10 ** long)));
 }
